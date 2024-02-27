84509fe5f9586e9618adf04d8761dbe098c2fb3a
Averkin Alexander <alexavkn@gmail.com>
2024-02-27 20:46:23 +0300

    feat: add mergeObjects task

--- src/objects-tasks.js
+++ src/objects-tasks.js
@@ -34,8 +34,20 @@ function shallowCopy(obj) {
  *    mergeObjects([{a: 1, b: 2}, {b: 3, c: 5}]) => {a: 1, b: 5, c: 5}
  *    mergeObjects([]) => {}
  */
-function mergeObjects(/* objects */) {
-  throw new Error('Not implemented');
+function mergeObjects(objects) {
+  const mergeObj = {};
+
+  objects.forEach((element) => {
+    Object.entries(element).forEach(([key, value]) => {
+      if (mergeObj[key]) {
+        mergeObj[key] += value;
+      } else {
+        mergeObj[key] = value;
+      }
+    });
+  });
+
+  return mergeObj;
 }
 
 /**
