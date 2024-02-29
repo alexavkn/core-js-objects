994a6a3e73e6ed40f564838362a264cd5d16c277
Averkin Alexander <alexavkn@gmail.com>
2024-03-01 00:54:59 +0300

    feat: add makeword selltickets task

--- src/objects-tasks.js
+++ src/objects-tasks.js
@@ -145,8 +145,17 @@ function makeImmutable(obj) {
  *    makeWord({ a: [0, 1], b: [2, 3], c: [4, 5] }) => 'aabbcc'
  *    makeWord({ H:[0], e: [1], l: [2, 3, 8], o: [4, 6], W:[5], r:[7], d:[9]}) => 'HelloWorld'
  */
-function makeWord(/* lettersObject */) {
-  throw new Error('Not implemented');
+function makeWord(lettersObject) {
+  const arrValues = Object.values(lettersObject);
+  const arrLetter = arrValues.flat();
+
+  Object.entries(lettersObject).forEach(([key, value]) => {
+    value.forEach((element) => {
+      arrLetter[element] = key;
+    });
+  });
+
+  return arrLetter.join('');
 }
 
 /**
@@ -163,8 +172,31 @@ function makeWord(/* lettersObject */) {
  *    sellTickets([25, 25, 50]) => true
  *    sellTickets([25, 100]) => false (The seller does not have enough money to give change.)
  */
-function sellTickets(/* queue */) {
-  throw new Error('Not implemented');
+function sellTickets(queue) {
+  const cashRegister = {
+    25: 0,
+    50: 0,
+  };
+
+  for (let i = 0; i < queue.length; i += 1) {
+    const element = queue[i];
+    switch (element) {
+      case 100:
+        cashRegister[50] -= 1;
+        cashRegister[25] -= 1;
+        break;
+      case 50:
+        cashRegister[50] += 1;
+        cashRegister[25] -= 1;
+        break;
+      default:
+        cashRegister[25] += 1;
+    }
+    if (cashRegister[25] < 0 || cashRegister[50] < 0) {
+      return false;
+    }
+  }
+  return true;
 }
 
 /**
